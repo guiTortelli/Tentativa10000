f01304f515911cd46ebbe2a55434f716b85cb440
JaqueProenca <99728859+JaquelineLucio@users.noreply.github.com>
2022-08-03 18:27:58 +0100

    Tentativa10000

---
+++ API/JobPortal_API/JobPortal_API/DTOs/AplicacaoTrabalhoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace JobPortal_API.DTOs
+{
+    public class AplicacaoTrabalhoDTO
+    {
+        public int IdAplicacao { get; set; }
+
+        public int IdOferta { get; set; }
+      
+        public int IdCandidato { get; set; }
+        public DateTime DataAplicacao { get; set; } = DateTime.Now;
+
+        public bool aplicacaoAceite { get; set; } = false;
+    }
+}
